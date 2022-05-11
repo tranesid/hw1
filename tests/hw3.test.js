ce4bb743ff6d20ef9dc3b082fab600e08a6c25c0
James <jimibue@gmail.com>
2022-05-10 17:35:48 -0700

    more tests

--- tests/hw3.test.js
+++ tests/hw3.test.js
@@ -2,7 +2,7 @@ const { doubleNum, doubleArray, isEven, filterEvens } = require("../sol/hw3");
 
 describe("doubling an array of numbers", () => {
   // test callback that doubles one number
-  it("test doubleNumb", () => {
+  it("test doubleNum", () => {
     let returnValue1 = doubleNum(2);
     let returnValue2 = doubleNum(10);
 
@@ -12,13 +12,15 @@ describe("doubling an array of numbers", () => {
 
   // test  doubling array of numbers
   it("test doubleArray", () => {
-    let returnValue1 = doubleArray([1,2,3]);
-    let returnValue2 = doubleArray([10,20,30]);
+    let arr = [1, 2, 3];
+    let returnValue1 = doubleArray(arr);
+    let returnValue2 = doubleArray([10, 20, 30]);
 
-    expect(returnValue1).toEqual([2,4,6]);
-    expect(returnValue2).toEqual([20,40,60]);
+    expect(returnValue1).toEqual([2, 4, 6]);
+    expect(returnValue2).toEqual([20, 40, 60]);
+    // original array should not be mutated
+    expect(arr).toEqual([1, 2, 3]);
   });
-
 });
 
 describe("filter evens from an array of numbers", () => {
@@ -33,14 +35,15 @@ describe("filter evens from an array of numbers", () => {
 
   // test  filter of even numbers
   it("test doubleArray", () => {
-    let returnValue1 = filterEvens([1,2,3,4]);
-    let returnValue2 = filterEvens([10,20,31]);
-    let returnValue3 = filterEvens([1,31]);
+    let arr = [1, 2, 3, 4];
+    let returnValue1 = filterEvens(arr);
+    let returnValue2 = filterEvens([10, 20, 31]);
+    let returnValue3 = filterEvens([1, 31]);
 
-    expect(returnValue1).toEqual([2,4]);
-    expect(returnValue2).toEqual([10,20]);
+    expect(returnValue1).toEqual([2, 4]);
+    expect(returnValue2).toEqual([10, 20]);
     expect(returnValue3).toEqual([]);
+    // original array should not be mutated
+    expect(arr).toEqual([1, 2, 3, 4]);
   });
-
 });
-
