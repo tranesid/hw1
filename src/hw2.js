bd54e1d6b057ee76bdf3aae68b1f5b909074cf45
James <jimibue@gmail.com>
2022-05-16 16:10:02 -0600

    h2 h3

--- src/hw2.js
+++ src/hw2.js
@@ -1,1 +1,56 @@
 
+// This 4 methods are trying to illustrate a couple ideas
+// Mutation: does your function change the things passed to it
+// Adding an item vs replacing in item in array.
+
+
+/**
+ * adds item to the array passed at given index
+ * @param {array} arr: the array to add item to
+ * @param {any} item: the item to add to array
+ * @param {number} index: the index to add to array
+ * @returns {undefined}
+ */
+ const addItemToArray = (arr, item, index)=>{
+   //TODO
+}
+
+/**
+ * adds item to a new array at given index
+ * @param {array} arr: the array to add item to
+ * @param {any} item: the item to add to array
+ * @param {number} index: the index to add to array
+ * @returns {array} a new array with item added
+ */
+const addItemToANewArray=(arr, item, index)=>{
+     //TODO
+}
+
+/**
+ * replace item to the array passed at given index
+ * @param {array} arr: the array to add item to
+ * @param {any} item: the item to add to array
+ * @param {number} index: the index to add to array
+ * @returns {undefined}
+ */
+ const replaceItemToArray = (arr, item, index)=>{
+       //TODO
+}
+
+/**
+ * replace item to a new array at given index
+ * @param {array} arr: the array to add item to
+ * @param {any} item: the item to add to array
+ * @param {number} index: the index to add to array
+ * @returns {array} a new array with item added
+ */
+const replaceItemToANewArray=(arr, item, index)=>{
+   //TODO
+}
+
+module.exports = {
+    addItemToArray,
+    addItemToANewArray,
+    replaceItemToArray,
+    replaceItemToANewArray
+  };
---
+++ src/hw3.js
@@ -0,0 +1,55 @@
+//1. write a function that takes an array of numbers
+// and return a new array with value doubled
+
+// part1 write a function that doubles a number and test
+/**
+ * doubles a number
+ * @param  {number} num
+ * @return  {number} number times 2
+ */
+ const doubleNum = (num) => {
+   //TODO
+  };
+  
+  // part 2 use the method from above to double numbers in new array
+  /**
+   * doubles numbers from given array into a new array
+   * @param  {[number]} arr: an array of numbers to be doubled
+   * @return {[number]} a NEW array with numbers doubled
+   */
+  const doubleArray = (arr) => {
+     //TODO
+  };
+  
+  //2. write a function that takes an array of numbers
+  // and return a new array with only the even ones
+  
+  // part1 write a function that takes number and return true if even
+  /**
+   * returns true if num is even false otherwise
+   * @param  {number} a integer
+   * @return  {boolean} true if number was even false otherwise
+   */
+  const isEven = (num) => {
+      //TODO
+  };
+  
+  // part 2 use the method from above to double numbers in new array
+  /**
+   * doubles numbers from given array into a new array
+   * @param  {[number]} arr: an array of numbers to be doubled
+   * @return {[number]} a NEW array with numbers doubled
+   */
+  const filterEvens = (arr) => {
+    //TODO
+  };
+  
+  isEven;
+  
+  module.exports = {
+    doubleNum,
+    doubleArray,
+    isEven,
+    filterEvens,
+  };
+  
