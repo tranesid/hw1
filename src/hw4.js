436844a86b1c972cdd4368146f67a420335c9494
Sidney <tranesidney@gmail.com>
2022-05-19 15:11:24 -0600

    hw4.js created

--- src/hw4.js
+++ src/hw4.js
@@ -28,3 +28,49 @@ const characters = [
       gender: 'male',
   },
 ];
+
+// MAP
+// Get an array of all names
+let myListOfNames = characters.map(user => {
+  return user.name;
+  //console.log(user.name);
+ }
+)
+
+
+
+
+
+
+// Get an array of all heights
+// Get an array of objects with just name and height properties
+// Get an array of all first names
+// REDUCE OR FOREACH
+// Get the total mass of all characters
+// Get the total height of all characters
+// Get the total number of characters in all the character names
+// Get the total number of characters by eye color (hint. a map of eye color to count)
+// FILTER
+// Get characters with mass greater than 100
+// Get characters with height less than 200
+// Get all male characters
+// Get all female characters
+// SORT
+// Sort by name
+// Sort by mass
+// Sort by height
+// Sort by gender
+// EVERY
+// Does every character have blue eyes?
+// Does every character have mass more than 40?
+// Is every character shorter than 200?
+// Is every character male?
+// SOME
+// Is there at least one male character?
+// Is there at least one character with blue eyes?
+// Is there at least one character taller than 200?
+// Is there at least one character that has mass less than 50?
+// Bonus
+// Redo the filter and map functions challenges, but do them with reduce/forEach
+// come up with your own and use different methods you have not used
+// add tests
